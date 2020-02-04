351cf5593f8ac2db9022a516f7bfb2cc82ebd8ca
YUKI NODA <59194511+Yuki0520-ba@users.noreply.github.com>
2020-02-04 23:02:51 +0900

    Add files via upload

--- weather_ping.js
+++ weather_ping.js
@@ -8,8 +8,13 @@
 'use strict';
 var https = require('https');
 
+//ユーザーの郵便番号を直書きで設定する
+var user_adress='102-0071';
+
 
 module.exports = (robot) => {
+
+  /////////////////////テスト//////////////////////
   robot.respond(/PING$/i, (res) => {
     res.send('PONG');
   });
@@ -26,19 +31,31 @@ module.exports = (robot) => {
     res.send(`Server time is: ${new Date()}`);
   });
 
+  robot.respond('map', (res) => {
+    var str=res.json.place.split(' ');
+    var str_result=str[0].split('〒');
+    
+    user_adress=str_result[1];
+    console.log(typeof(user_adress));
+    res.send(`あなたの住所を`+user_adress+'に変更しました。');
+  });
+
+//////////////////////////////////////////////////////////
+
 
   
+
+
 
   //open wether Mapから今日の天気情報を取得してボットに返す
   robot.respond(/おはよう$/i, (res1) => {
     var http = require('http');
 
-    var zip='102-0071,jp';
-    
-    //var zip=res1.match[1]+',jp';
+    var zip=user_adress+',jp';
     var units = 'metric';
     var APIKEY = "87e3d13be8431fcf4ccedf1479ff2064";
     var URL = 'http://api.openweathermap.org/data/2.5/weather?zip='+ zip +'&units='+ units +'&lang=ja&'+'&appid='+ APIKEY;
+    
     http.get(URL, function(res) {
       var body = '';
       res.setEncoding('utf8');
@@ -47,10 +64,7 @@ module.exports = (robot) => {
       });
       res.on('data', function(chunk) {
         res = JSON.parse(body);
-
-        //res1.send(String(res.name));
-        res1.send("今日の天気は　"+String(res.weather[0].description)+"　です!");
-        
+        res1.send("今日の"+String(res.name)+"の天気は　"+String(res.weather[0].description)+"　です!");        
       });
     }).on('error', function(e) {
       console.log(e.message);
@@ -63,8 +77,7 @@ module.exports = (robot) => {
   robot.respond(/おやすみ$/i, (res1) => {
     //api叩く
     var http = require('http');
-    var zip='102-0071,jp'; 
-    //var zip=res1.match[1]+',jp';
+    var zip=user_adress+',jp';
     var units = 'metric';
     var APIKEY = "87e3d13be8431fcf4ccedf1479ff2064";
     var URL = 'http://api.openweathermap.org/data/2.5/forecast?zip='+ zip +'&units='+ units +'&lang=ja&'+'&appid='+ APIKEY;
@@ -94,11 +107,12 @@ module.exports = (robot) => {
         format_str = format_str.replace(/DD/g, tomorrow_date);
 
         //明日の９時時点の天気を返す
-        for(var n=0;n<res.cnt;n++)
+        for(var n=0;n<res.cnt;n++){
           if(res.list[n].dt_txt.match(format_str)){
             if(res.list[n].dt_txt.match("09:00:00"))
               res1.send("明日の朝の天気は "+res.list[n].weather[0].description+" です!");
-            }  
+            }
+          }  
         
       });
     }).on('error', function(e) {
